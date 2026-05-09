5985862fce0a2e3027a0ced055123ca9f280726e
ichirou-haruyama <ichirou.haruyama@gmail.com>
2026-05-10 01:31:54 +0900

    Configure Firebase project

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCihbIoemKxogFdd78b8C8MZQZZAfzK8Mw",
+  authDomain: "attendance-app-ba293.firebaseapp.com",
+  projectId: "attendance-app-ba293",
+  storageBucket: "attendance-app-ba293.firebasestorage.app",
+  messagingSenderId: "256765530683",
+  appId: "1:256765530683:web:a4489c03456188f406c21e",
+  measurementId: "G-9QLCQ78880",
 };
 
 export const attendanceDocumentPath = "attendanceApps/default";
